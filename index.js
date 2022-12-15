6011e02a7110b62dd9e8dcd19c192d09626b31fb
+ <+>
2022-12-15 10:42:11 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // We will need two interations: one for the number and the other for the complement
+  //iterate over the array of numbers
+  for(let i = 0; i<array.length; i++){
+    //for the current number, identify a complementary number that adds to our target
+  //example is if our number is 2 and target is 5, the complementary number is 3
+    const complement = target-array[i];
+  //iterate over the remaining numbers in the array
+    for(let j= i+1; j<array.length; j++){
+    //check if any of the remaining number is the complement
+    //if so, return true
+      if(array[j] ===complement)return true;
+
+   }
+  }
+  //if we reach the end of the array,without finding complement,return false
+   return false;
+
+
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Quadratic.  The algorithm needs to make n**2 iterations
 */
 
 /* 
   Add your pseudocode here
+  iterate over the array of numbers
+  for the current number, identify a comnplementary mumber that adds to our target
+  example is if our number is 2 and target is 5, the completary number is 3
+  iterate over the remaining numbers in the array
+    check if any of the remaining number is the complement
+       if so, return true
+  if we reach the end of the array, return false
 */
 
 /*
